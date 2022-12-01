1e8177c99098035c0001b55e77e6e39342cdf09b
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2022-12-01 11:09:40 +0300

    Update Program.cs

--- 9- Updating_Related_Data/Updating_Related_Data/Updating_Related_Data/Program.cs
+++ 9- Updating_Related_Data/Updating_Related_Data/Updating_Related_Data/Program.cs
@@ -229,7 +229,6 @@ class ApplicationDbContext : DbContext
     {
         optionsBuilder.UseSqlServer("Server=localhost, 1433;Database=ApplicationDb;User ID=SA;Password=1q2w3e4r+!");
     }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Address>()
