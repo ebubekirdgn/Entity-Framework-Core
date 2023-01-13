e45e04ceaca588341e36fbd6890c2fc02e8a2fbb
Ebubekir Dogan <ebubekir.dogan@bil.omu.edu.tr>
2023-01-13 21:37:04 +0300

    update

---
+++ 27- Views/Generics/Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+List<string> deneme = new List<string>();
+MyList<string> deneme2 = new MyList<string>();  
+deneme2.Add()
+
+ class MyList<T>
+{
+    public void Add(T item)
+    {
+
+    }
+}
